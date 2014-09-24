5147a266585b3abf05a81f62c581c38bc13f3d4a
James Garfield <jgarfield@adblocks.com>
2014-09-24 12:03:37 -0400

    Somewhat more robust search for clone url.

--- GitHubSourceTree.user.js
+++ GitHubSourceTree.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       GitHubSourceTree
 // @namespace  http://really-serious.biz/
-// @version    1.0.3
+// @version    1.0.4
 // @description  Adds a "Clone in SourceTree" button to github pages
 // @respository  https://github.com/jamesgarfield/GitHubSourceTree
 // @match https://*github.com/*
@@ -12,21 +12,18 @@
 (function(){
     const $ = document.querySelectorAll.bind(document);
     
+    //GitHub's "Clone in Desktop" Button
     const gitHubNode = $(".clone-options + a")[0]
-
-    if (!gitHubNode) {
-    	return;
-    }
-
     const parentNode = gitHubNode.parentNode;
+    //Insert our button between the GitHub Clone button and whatever is after it.
     const insertBeforeNode = gitHubNode.nextSibling;
-    const gitURL = $(".clone-url-box .js-url-field")[0].value
+    //Find the clone url for this repo
+    const gitURL = $(".js-url-field")[0].value
     
     var sourceTreeNode = gitHubNode.cloneNode();
     sourceTreeNode.href = 'sourcetree://cloneRepo/' + gitURL;
     sourceTreeNode.innerHTML = '<span class="octicon octicon-device-desktop"></span>Clone in SourceTree';
     
     parentNode.insertBefore(sourceTreeNode, insertBeforeNode);
-    
 })()
 
